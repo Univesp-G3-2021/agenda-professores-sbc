d799d7052dbc51d70486ab1e232b83a4447ab875
Marcos Lohmann <marcos.lohmann@gmail.com>
2021-10-04 21:26:52 -0300

    atvação do widget do calendário

--- view/js/main.js
+++ view/js/main.js
@@ -36,8 +36,40 @@ $(function(){
     });
     var _loadHome = function(){
         $('body').empty().load('/view/home.html',{},function(){
-            console.log("pronto pra carregar agenda");
-            $("#agenda").fullCalendar('option', 'height', $("#nav-tabContent").innerHeight()-5);
+            $("#agenda").fullCalendar({
+                header: {
+                    left: "prev,next today",
+                    center: "title",
+                    right: "month,agendaWeek,agendaDay,listMonth"
+                },
+                defaultView: "agendaWeek",
+                selectable: true,
+                locale: "pt-br",
+                buttonIcons: true,
+                weekNumbers: true,
+                navLinks: true, 
+                editable: true,
+                eventLimit: true, 
+                minTime: "00:00:00",
+                maxTime: "23:59:59",
+                scrollTime: "06:00:00",
+                slotEventOverlap: false,
+                businessHours: {
+                    daysOfWeek: [ 1, 2, 3, 4, 5 ],
+                    startTime: '07:00',
+                    endTime: '17:00',
+                },
+                windowResize: function(view) {
+                    $("#pane_main_agenda .body #grid-agenda").fullCalendar('option', 'height', $("#nav-tabContent").innerHeight()-5);                        
+                },
+                eventSources: [],
+                events:[{
+                    id: "999",
+                    title: "Sample Event",
+                    start: "2021-10-06T09:30:00",
+                    end: "2021-10-06T10:30:00"
+                }]
+            });
         });
     }
     _loadHome();
