f5ba6326f65ebe6210c36bf566aac7b0a20b3aa5
mcloh <marcos.lohmann@gmail.com>
2021-11-27 18:57:50 -0300

    implementar gatilho onload em DOMs

--- control/js/classes.js
+++ control/js/classes.js
@@ -27,7 +27,7 @@ var loadPrecisoVolanteGrid = function(){
 var loadSelectClasses = function(_esc_codigo){
     $.get("/model/apsbc_model.php?className=Classe&methodName=listByEscola&arguments="+_esc_codigo,{},function(data){
         $("#sel_cls_codigo").empty();
-        $("#sel_cls_codigo").append("<option value='' disabled>-- escolha uma turma</option>");
+        $("#sel_cls_codigo").append("<option value=''>-- escolha uma turma</option>");
         for(var doc of data){
             $("#sel_cls_codigo").append("<option value='"+doc.cls_codigo+"'>"+doc.cls_descricao+" - "+doc.cls_turno+" (Prof. "+doc.prf_nome+")</option>");
         }
