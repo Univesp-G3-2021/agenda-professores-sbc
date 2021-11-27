23060b61b28572996a1796dafd18d00aacdee3ec
mcloh <marcos.lohmann@gmail.com>
2021-11-26 23:15:08 -0300

    fix mysql connetcion

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,14 +1,14 @@
 <?php
 class DB{
-	private $servername = ini_get('mysqli.default_host');
-	private $username = ini_get('mysqli.default_user');
-	private $password = ini_get('mysqli.default_pw');
-	private $database = ini_get('mysqli.default_user');
+	//private $servername = ini_get('mysqli.default_host');
+	//private $username = ini_get('mysqli.default_user');
+	//private $password = ini_get('mysqli.default_pw');
+	//private $database = ini_get('mysqli.default_user');
 	private $conn = null;
 
 	public function getConn(){
 		if(is_null($this->conn)){
-			$this->conn = new mysqli($this->servername, $this->username, $this->password);
+			$this->conn = new mysqli(ini_get('mysqli.default_host'), ini_get('mysqli.default_user'), ini_get('mysqli.default_pw'), ini_get('mysqli.default_user'));
 			if ($this->conn->connect_errno) {
 				error_log("Failed to connect to MySQL: ".$this->conn->connect_error, 0);
 			}
