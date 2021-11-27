d17360a4a307f1a1829218453fe632626491dfc5
mcloh <marcos.lohmann@gmail.com>
2021-11-27 14:14:48 -0300

    traduzindo grids

--- control/js/secretaria.js
+++ control/js/secretaria.js
@@ -8,7 +8,15 @@ var modSecretaria = {
                 cache: false
             },
             url: "/model/apsbc_model.php?className=Agenda&methodName=gridSolicitacoes&arguments=1,10",
-            formatters:{}
+            formatters:{},
+            labels:{
+                noResults: "sem registros",
+                infos: "exibindo {{ctx.start}} a {{ctx.end}} de {{ctx.total}} registro(s)",
+                loading: "carregando do banco de dados...",
+                all: "todos",
+                refres: "recarregar",
+                search: "localizar"
+            }
         }).on("load.rs.jquery.bootgrid", function (e)
         {
             console.log(e);
