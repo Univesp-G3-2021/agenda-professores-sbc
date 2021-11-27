83e0bd3eaecb49012644adf31d31f3428fc4fb94
mcloh <marcos.lohmann@gmail.com>
2021-11-26 23:46:35 -0300

    incluindo camada do modelo

--- model/professor.php
+++ model/professor.php
@@ -2,6 +2,26 @@
 header('Content-Type: application/json; charset=utf-8');
 include_once '../includes/mysql.php';
 
-$res = MySQL::query('SELECT * FROM professores');
-echo json_encode($res);
+class Professor{
+
+    public static function listAll(){
+        $res = MySQL::query('SELECT * FROM professores');
+        echo json_encode($res);
+    }
+
+    public static function listByEscola($esc_codigo){
+        $res = MySQL::query("SELECT * FROM professores WHERE esc_codigo='$esc_codigo'");
+        echo json_encode($res);
+    }
+
+    public static function getByClasse($cls_codigo){
+        $cls = MySQL::query("SELECT * FROM classe WHERE cls_codigo='$cls_codigo'");
+        $prf_codigo = $cls[0]['prf_codigo'];
+        $res = MySQL::query("SELECT * FROM professores WHERE prf_codigo='$prf_codigo'");
+        echo json_encode($res);
+    }
+
+}
+
+Professor::getByClasse('teste002');
 ?>
