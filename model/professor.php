cf1ff7628e66bfc29ce5e69fcedc00fc0b7356b1
mcloh <marcos.lohmann@gmail.com>
2021-11-26 23:16:19 -0300

    fix mysql connetcion

--- model/professor.php
+++ model/professor.php
@@ -2,6 +2,6 @@
 header('Content-Type: application/json; charset=utf-8');
 include_once '../includes/mysql.php';
 
-$res = MySQL()->query('SELECT * FROM professores');
+$res = MySQL->query('SELECT * FROM professores');
 echo json_encode($res);
 ?>
