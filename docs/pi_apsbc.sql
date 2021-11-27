1ae63cdcf0ee70e1b845b78deba977b923a203cc
mcloh <marcos.lohmann@gmail.com>
2021-11-27 08:41:45 -0300

    update DDL file

--- docs/pi_apsbc.sql
+++ docs/pi_apsbc.sql
@@ -145,3 +145,229 @@ ALTER TABLE `solicitacoes`
 
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`acl_grupo`) REFERENCES `acl` (`acl_grupo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE VIEW `agenda` AS SELECT
+    `solicitacoes`.`sol_codigo` AS `sol_codigo`,
+    `solicitacoes`.`prf_codigo` AS `prf_codigo`,
+    `solicitacoes`.`sol_datahora` AS `sol_datahora`,
+    `solicitacoes`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `solicitacoes`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `solicitacoes`.`cls_codigo` AS `cls_codigo`,
+    `solicitacoes`.`sol_motivo` AS `sol_motivo`,
+    `solicitacoes`.`sol_comprovante` AS `sol_comprovante`,
+    `solicitacoes`.`sol_atendida` AS `sol_atendida`,
+    `solicitacoes`.`sol_ativa` AS `sol_ativa`,
+    `movimentos`.`prf_codigo` AS `prf_volante`
+FROM
+    (
+        `solicitacoes`
+    JOIN `movimentos` ON
+        (
+            (
+                `solicitacoes`.`sol_codigo` = `movimentos`.`sol_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_classe` AS SELECT
+    `agenda`.`sol_codigo` AS `sol_codigo`,
+    `agenda`.`prf_codigo` AS `prf_codigo`,
+    `agenda`.`sol_datahora` AS `sol_datahora`,
+    `agenda`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda`.`cls_codigo` AS `cls_codigo`,
+    `agenda`.`sol_motivo` AS `sol_motivo`,
+    `agenda`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda`.`sol_atendida` AS `sol_atendida`,
+    `agenda`.`sol_ativa` AS `sol_ativa`,
+    `agenda`.`prf_volante` AS `prf_volante`,
+    `classe`.`esc_codigo` AS `esc_codigo`,
+    `classe`.`cls_turno` AS `cls_turno`,
+    `classe`.`cls_descricao` AS `cls_descricao`
+FROM
+    (
+        `agenda`
+    JOIN `classe` ON
+        (
+            (
+                `agenda`.`cls_codigo` = `classe`.`cls_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_classe_solicitacao` AS SELECT
+    `agenda_solicitacoes`.`sol_codigo` AS `sol_codigo`,
+    `agenda_solicitacoes`.`prf_codigo` AS `prf_codigo`,
+    `agenda_solicitacoes`.`sol_datahora` AS `sol_datahora`,
+    `agenda_solicitacoes`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda_solicitacoes`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda_solicitacoes`.`cls_codigo` AS `cls_codigo`,
+    `agenda_solicitacoes`.`sol_motivo` AS `sol_motivo`,
+    `agenda_solicitacoes`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda_solicitacoes`.`sol_atendida` AS `sol_atendida`,
+    `agenda_solicitacoes`.`sol_ativa` AS `sol_ativa`,
+    `classe`.`esc_codigo` AS `esc_codigo`,
+    `classe`.`cls_turno` AS `cls_turno`,
+    `classe`.`cls_descricao` AS `cls_descricao`
+FROM
+    (
+        `agenda_solicitacoes`
+    JOIN `classe` ON
+        (
+            (
+                `agenda_solicitacoes`.`cls_codigo` = `classe`.`cls_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_escola` AS SELECT
+    `agenda_classe`.`sol_codigo` AS `sol_codigo`,
+    `agenda_classe`.`prf_codigo` AS `prf_codigo`,
+    `agenda_classe`.`sol_datahora` AS `sol_datahora`,
+    `agenda_classe`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda_classe`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda_classe`.`cls_codigo` AS `cls_codigo`,
+    `agenda_classe`.`sol_motivo` AS `sol_motivo`,
+    `agenda_classe`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda_classe`.`sol_atendida` AS `sol_atendida`,
+    `agenda_classe`.`sol_ativa` AS `sol_ativa`,
+    `agenda_classe`.`prf_volante` AS `prf_volante`,
+    `agenda_classe`.`esc_codigo` AS `esc_codigo`,
+    `agenda_classe`.`cls_turno` AS `cls_turno`,
+    `agenda_classe`.`cls_descricao` AS `cls_descricao`,
+    `escolas`.`esc_nome` AS `esc_nome`,
+    `escolas`.`esc_cep` AS `esc_cep`,
+    `escolas`.`esc_logradouro` AS `esc_logradouro`,
+    `escolas`.`esc_bairro` AS `esc_bairro`,
+    `escolas`.`esc_numero` AS `esc_numero`
+FROM
+    (
+        `agenda_classe`
+    JOIN `escolas` ON
+        (
+            (
+                `agenda_classe`.`esc_codigo` = `escolas`.`esc_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_escola_solicitacao` AS SELECT
+    `agenda_classe_solicitacao`.`sol_codigo` AS `sol_codigo`,
+    `agenda_classe_solicitacao`.`prf_codigo` AS `prf_codigo`,
+    `agenda_classe_solicitacao`.`sol_datahora` AS `sol_datahora`,
+    `agenda_classe_solicitacao`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda_classe_solicitacao`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda_classe_solicitacao`.`cls_codigo` AS `cls_codigo`,
+    `agenda_classe_solicitacao`.`sol_motivo` AS `sol_motivo`,
+    `agenda_classe_solicitacao`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda_classe_solicitacao`.`sol_atendida` AS `sol_atendida`,
+    `agenda_classe_solicitacao`.`sol_ativa` AS `sol_ativa`,
+    `agenda_classe_solicitacao`.`esc_codigo` AS `esc_codigo`,
+    `agenda_classe_solicitacao`.`cls_turno` AS `cls_turno`,
+    `agenda_classe_solicitacao`.`cls_descricao` AS `cls_descricao`,
+    `escolas`.`esc_nome` AS `esc_nome`,
+    `escolas`.`esc_cep` AS `esc_cep`,
+    `escolas`.`esc_logradouro` AS `esc_logradouro`,
+    `escolas`.`esc_bairro` AS `esc_bairro`,
+    `escolas`.`esc_numero` AS `esc_numero`
+FROM
+    (
+        `agenda_classe_solicitacao`
+    JOIN `escolas` ON
+        (
+            (
+                `agenda_classe_solicitacao`.`esc_codigo` = `escolas`.`esc_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_solicitacoes` AS SELECT
+    `solicitacoes`.`sol_codigo` AS `sol_codigo`,
+    `solicitacoes`.`prf_codigo` AS `prf_codigo`,
+    `solicitacoes`.`sol_datahora` AS `sol_datahora`,
+    `solicitacoes`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `solicitacoes`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `solicitacoes`.`cls_codigo` AS `cls_codigo`,
+    `solicitacoes`.`sol_motivo` AS `sol_motivo`,
+    `solicitacoes`.`sol_comprovante` AS `sol_comprovante`,
+    `solicitacoes`.`sol_atendida` AS `sol_atendida`,
+    `solicitacoes`.`sol_ativa` AS `sol_ativa`
+FROM
+    `solicitacoes`
+WHERE
+    (
+        NOT(
+            `solicitacoes`.`sol_codigo` IN(
+            SELECT
+                `movimentos`.`sol_codigo`
+            FROM
+                `movimentos`
+        )
+        )
+    )
+
+CREATE VIEW `agenda_solicitacoes_abertas` AS SELECT
+    `agenda_escola_solicitacao`.`sol_codigo` AS `sol_codigo`,
+    `agenda_escola_solicitacao`.`prf_codigo` AS `prf_codigo`,
+    `agenda_escola_solicitacao`.`sol_datahora` AS `sol_datahora`,
+    `agenda_escola_solicitacao`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda_escola_solicitacao`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda_escola_solicitacao`.`cls_codigo` AS `cls_codigo`,
+    `agenda_escola_solicitacao`.`sol_motivo` AS `sol_motivo`,
+    `agenda_escola_solicitacao`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda_escola_solicitacao`.`sol_atendida` AS `sol_atendida`,
+    `agenda_escola_solicitacao`.`sol_ativa` AS `sol_ativa`,
+    `agenda_escola_solicitacao`.`esc_codigo` AS `esc_codigo`,
+    `agenda_escola_solicitacao`.`cls_turno` AS `cls_turno`,
+    `agenda_escola_solicitacao`.`cls_descricao` AS `cls_descricao`,
+    `agenda_escola_solicitacao`.`esc_nome` AS `esc_nome`,
+    `agenda_escola_solicitacao`.`esc_cep` AS `esc_cep`,
+    `agenda_escola_solicitacao`.`esc_logradouro` AS `esc_logradouro`,
+    `agenda_escola_solicitacao`.`esc_bairro` AS `esc_bairro`,
+    `agenda_escola_solicitacao`.`esc_numero` AS `esc_numero`,
+    `professores`.`prf_nome` AS `prf_nome`,
+    `professores`.`prf_documento` AS `prf_documento`,
+    `professores`.`prf_disciplinas` AS `prf_disciplinas`
+FROM
+    (
+        `agenda_escola_solicitacao`
+    JOIN `professores` ON
+        (
+            (
+                `agenda_escola_solicitacao`.`prf_codigo` = `professores`.`prf_codigo`
+            )
+        )
+    )
+
+CREATE VIEW `agenda_volantes` AS SELECT
+    `agenda_escola`.`sol_codigo` AS `sol_codigo`,
+    `agenda_escola`.`prf_codigo` AS `prf_codigo`,
+    `agenda_escola`.`sol_datahora` AS `sol_datahora`,
+    `agenda_escola`.`sol_agenda_inicio` AS `sol_agenda_inicio`,
+    `agenda_escola`.`sol_agenda_termino` AS `sol_agenda_termino`,
+    `agenda_escola`.`cls_codigo` AS `cls_codigo`,
+    `agenda_escola`.`sol_motivo` AS `sol_motivo`,
+    `agenda_escola`.`sol_comprovante` AS `sol_comprovante`,
+    `agenda_escola`.`sol_atendida` AS `sol_atendida`,
+    `agenda_escola`.`sol_ativa` AS `sol_ativa`,
+    `agenda_escola`.`prf_volante` AS `prf_volante`,
+    `agenda_escola`.`esc_codigo` AS `esc_codigo`,
+    `agenda_escola`.`cls_turno` AS `cls_turno`,
+    `agenda_escola`.`cls_descricao` AS `cls_descricao`,
+    `agenda_escola`.`esc_nome` AS `esc_nome`,
+    `agenda_escola`.`esc_cep` AS `esc_cep`,
+    `agenda_escola`.`esc_logradouro` AS `esc_logradouro`,
+    `agenda_escola`.`esc_bairro` AS `esc_bairro`,
+    `agenda_escola`.`esc_numero` AS `esc_numero`,
+    `professores`.`prf_nome` AS `prf_volante_nome`,
+    `professores`.`prf_documento` AS `prf_volante_documento`
+FROM
+    (
+        `agenda_escola`
+    JOIN `professores` ON
+        (
+            (
+                `agenda_escola`.`prf_volante` = `professores`.`prf_codigo`
+            )
+        )
+    )
